14bb7a44d6186cc9fd4b0156b845d774450f974e
chrisemmanuel <chris.emmanuel@hotmail.com>
2023-10-17 21:44:52 +0100

    Part 1 challenge

---
+++ part_1/character.py
@@ -0,0 +1,10 @@
+# Kevin Hart
+character = dict()
+character["name"] = str('Kevin Haart')
+
+character["age"] = int(34)
+
+character["net_worth_millions"]  = float(6.7)
+
+if __name__ == "__main__":
+    print(character)
